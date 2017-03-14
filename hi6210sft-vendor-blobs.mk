1576e9046063b40c4f334f4fdd9dc7561f61e798
hak86 <hakamelassouad@gmail.com>
2017-03-14 01:58:24 +0100

    hisi: copy codecs

--- hi6210sft-vendor-blobs.mk
+++ hi6210sft-vendor-blobs.mk
@@ -7,3 +7,11 @@ PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/etc/sws,system/etc/sws) \
 	$(LOCAL_PATH)/etc/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/etc/audio_policy.conf:system/etc/audio_policy.conf \
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/etc/media_codecs.xml:system/etc/media_codecs.xml \
+        $(LOCAL_PATH)/etc/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+        $(LOCAL_PATH)/etc/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+        $(LOCAL_PATH)/etc/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+        $(LOCAL_PATH)/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+        $(LOCAL_PATH)/etc/media_profiles.xml:system/etc/media_profiles.xml \
